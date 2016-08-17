bde8870387dd236789519724cd6718521d747dd7
cahrsjo <cahrsjo@gmail.com>
2016-08-17 14:38:50 +0200

    Add RavenDB setup

--- ReactJSDotNetRaven/Controllers/HomeController.cs
+++ ReactJSDotNetRaven/Controllers/HomeController.cs
@@ -2,35 +2,13 @@
 using System.Web.Mvc;
 using ReactDemo.Models;
 using System.Web.UI;
+using Raven.Client;
+using Raven.Client.Document;
 
 namespace ReactDemo.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly IList<CommentModel> _comments;
-
-        static HomeController()
-        {
-            _comments = new List<CommentModel>
-            {
-                new CommentModel
-                {
-                    Author = "Daniel Lo Nigro",
-                    Text = "Hello ReactJS.NET World!"
-                },
-                new CommentModel
-                {
-                    Author = "Pete Hunt",
-                    Text = "This is one comment"
-                },
-                new CommentModel
-                {
-                    Author = "Jordan Walke",
-                    Text = "This is *another* comment"
-                },
-            };
-        }
-
         public ActionResult Index()
         {
             return View(_comments);
@@ -39,14 +17,56 @@ namespace ReactDemo.Controllers
         [HttpPost]
         public ActionResult AddComment(CommentModel comment)
         {
-            _comments.Add(comment);
+            using (IDocumentStore store = new DocumentStore
+            {
+                Url = "http://localhost:8080/", // server URL
+                DefaultDatabase = "Comments"   // default database
+            })
+            {
+                store.Initialize(); // initializes document store, by connecting to server and downloading various configurations
+
+                using (IDocumentSession session = store.OpenSession()) // opens a session that will work in context of 'DefaultDatabase'
+                {
+                    session.Store(comment); // stores employee in session, assigning it to a collection `Employees`
+                    int commentId = comment.Id; // Session.Store will assign Id to employee, if it is not set
+
+                    session.SaveChanges(); // sends all changes to server
+                }
+            }
             return Content("Success :)");
         }
 
         [OutputCache(Location = OutputCacheLocation.None)]
         public ActionResult Comments()
         {
-            return Json(_comments, JsonRequestBehavior.AllowGet);
+            using (IDocumentStore store = new DocumentStore
+            {
+                Url = "http://localhost:8080/", // server URL
+                DefaultDatabase = "Comments"   // default database
+            })
+            {
+                store.Initialize(); // initializes document store, by connecting to server and downloading various configurations
+
+                using (IDocumentSession session = store.OpenSession()) // opens a session that will work in context of 'DefaultDatabase'
+                {
+                    CommentModel comment = new CommentModel
+                    {
+                        Author = "Calle",
+                        Text = "Hello from Calle"
+                    };
+
+                    session.Store(comment); // stores employee in session, assigning it to a collection `Employees`
+                    int commentId = comment.Id; // Session.Store will assign Id to employee, if it is not set
+
+                    session.SaveChanges(); // sends all changes to server
+
+                    // Session implements Unit of Work pattern,
+                    // therefore employee instance would be the same and no server call will be made
+                    CommentModel loadedComment = session.Load<CommentModel>(commentId);
+                    return Json(new List<CommentModel> { comment }, JsonRequestBehavior.AllowGet);
+                }
+            }
+           
         }
     }
 }
--- ReactJSDotNetRaven/Models/CommentModel.cs
+++ ReactJSDotNetRaven/Models/CommentModel.cs
@@ -7,6 +7,7 @@ namespace ReactDemo.Models
 {
     public class CommentModel
     {
+        public int Id { get; set; }
         public string Author { get; set; }
         public string Text { get; set; }
     }
